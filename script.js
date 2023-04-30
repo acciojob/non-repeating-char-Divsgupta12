c299fc18faed6efb9b7d5a6125cad6b7f3430895
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-04-30 13:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let str = prompt("enter the string");
+	function firstNonRepeatedCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+    if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
+      return char;
+    }
+  }
+  return null;
+}
